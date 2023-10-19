b08d51c149a15f15106747192e2bc0923f288b30
MoNaSser8 <11410120200479@stud.cu.edu.eg>
2023-10-20 01:52:39 +0300

    appbar

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:notes_th/views/widgets/notes_view_body.dart';
 
 class NotesView extends StatelessWidget {
   const NotesView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body:NotesViewBody(),
+    );
   }
 }
+
