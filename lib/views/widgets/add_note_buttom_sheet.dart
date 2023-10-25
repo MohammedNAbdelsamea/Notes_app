3201ab622ed0c315c0aadbdd745fbd2725adb7d2
MoNaSser8 <11410120200479@stud.cu.edu.eg>
2023-10-25 16:19:21 +0300

    custom_text_field.

--- lib/views/widgets/add_note_buttom_sheet.dart
+++ lib/views/widgets/add_note_buttom_sheet.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/cupertino.dart';
+import 'package:notes_th/views/widgets/custom_text_field.dart';
 
 class AddButtonSheet extends StatelessWidget {
   const AddButtonSheet({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  Container();
+    return  Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: Column(
+        children: [
+          SizedBox(height: 32,),
+          CustomTextField(),
+        ],
+      ),
+    );
   }
 }
