9e9e214e094332a07a677c28b4f190e8fb85f684
MoNaSser8 <11410120200479@stud.cu.edu.eg>
2023-10-27 20:22:27 +0200

    create add note cubit

---
+++ lib/cubits/add_note_cubit/add_note_state.dart
@@ -0,0 +1,10 @@
+
+abstract class AddNoteState{}
+  class AddNoteInitial extends AddNoteState{}
+  class AddNoteLoading extends AddNoteState{}
+  class AddNoteSuccess extends AddNoteState{}
+  class AddNoteFailure extends AddNoteState{
+  final String errMessage;
+
+  AddNoteFailure(this.errMessage);
+  }
