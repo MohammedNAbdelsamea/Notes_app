6f9483bb86d03631f327c46ab155ca0475bfd120
MoNaSser8 <11410120200479@stud.cu.edu.eg>
2023-11-29 18:04:46 +0200

    colors_list_view part1

--- lib/views/widgets/add_note_form.dart
+++ lib/views/widgets/add_note_form.dart
@@ -7,6 +7,7 @@ import 'package:intl/intl.dart';
 import 'package:notes_th/cubits/add_note_cubit/add_note_cubit.dart';
 import 'package:notes_th/cubits/add_note_cubit/add_note_state.dart';
 import 'package:notes_th/models/note_model.dart';
+import 'package:notes_th/views/widgets/colors_list_view.dart';
 
 import '../../constants.dart';
 import 'custom_button.dart';
@@ -47,6 +48,7 @@ class _AddNoteFormState extends State<AddNoteForm> {
             maxLines: 5,
           ),
           const SizedBox(height: 32,),
+          ColorsListView(),
           BlocBuilder <AddNoteCubit,AddNoteState>(
 
             builder: (context,state){
